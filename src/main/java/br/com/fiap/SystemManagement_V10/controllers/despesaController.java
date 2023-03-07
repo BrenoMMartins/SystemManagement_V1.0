333b379729e39bb2a8130d55e74638d6858071af
BrenoMMassa <brenommassa@gmail.coma>
2023-03-06 22:26:09 -0300

    criação requests class e add starts devtools pom

--- src/main/java/br/com/fiap/SystemManagement_V10/controllers/despesaController.java
+++ src/main/java/br/com/fiap/SystemManagement_V10/controllers/despesaController.java
@@ -1,5 +1,16 @@
 package br.com.fiap.SystemManagement_V10.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class despesaController {
     
+    @RequestMapping("/api/despesas")
+    @ResponseBody
+    public String show(){
+        return "despesa";
+    }
+
 }
