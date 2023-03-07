fab64874ba0ba54350d60c0bc896c14d577781e5
BrenoMMassa <brenommassa@gmail.coma>
2023-03-06 22:50:10 -0300

    +atributos,const,GetSet e ajustando RestController

--- src/main/java/br/com/fiap/SystemManagement_V10/controllers/despesaController.java
+++ src/main/java/br/com/fiap/SystemManagement_V10/controllers/despesaController.java
@@ -2,13 +2,19 @@ package br.com.fiap.SystemManagement_V10.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import br.com.fiap.SystemManagement_V10.controllers.models.Despesa;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@RestController
 public class despesaController {
     
     @GetMapping("/api/despesas")
-    public String show(){
-        return "Despesas do System";
+    public Despesa show(){
+        Despesa despesa = new Despesa((new BigDecimal(100)), LocalDate.now(), "cinema");
+        return despesa;
     }
 
 }
