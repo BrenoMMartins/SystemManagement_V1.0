b1686a173170482d2772c77d42e0f76eec76d759
mouragus <gumora18@hotmail.com>
2023-03-13 18:47:44 -0300

    arrumando problemas

--- src/main/java/br/com/fiap/SystemManagement_V10/controllers/TarefaController.java
+++ src/main/java/br/com/fiap/SystemManagement_V10/controllers/TarefaController.java
@@ -1,20 +1,20 @@
 package br.com.fiap.SystemManagement_V10.controllers;
 
-import java.math.BigDecimal;
+
 import java.time.LocalDate;
 
-import org.springframework.stereotype.Controller;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.fiap.SystemManagement_V10.controllers.models.Tarefa;
 
-@Controller
+@RestController
 public class TarefaController {
     
-    @GetMapping("/api/tarefa")
+    @GetMapping ("/api/tarefa")
     public Tarefa show(){
-        return new Tarefa = new Tarefa("Plano de Voo", LocalDate.now(), "Tarefa de Digital Entreprise");
+        return new Tarefa ("Plano de Voo", "Tarefa de Digital Entreprise",  LocalDate.now());
     }
 
 }
