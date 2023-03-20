ef086a1d443fc462a9368ce3329b753f0aec0db5
mouragus <gumora18@hotmail.com>
2023-03-20 19:32:50 -0300

    subindo alterações usuario

--- src/main/java/br/com/fiap/SystemManagement_V10/controllers/UsuarioController.java
+++ src/main/java/br/com/fiap/SystemManagement_V10/controllers/UsuarioController.java
@@ -1,5 +1,26 @@
 package br.com.fiap.SystemManagement_V10.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+
+
 import br.com.fiap.SystemManagement_V10.controllers.models.Usuario;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,9 +29,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UsuarioController {
     
+    Logger log = LoggerFactory.getLogger(UsuarioController.class);
+
+    List<Usuario> usuarios = new ArrayList <>();
+    
+    
     @GetMapping ("/api/usuario")
-    public Usuario show(){
-        return new Usuario ("mouratins", "Breno Moura", "mouratins@hotmail.com", "11944466401", "ilovc0de");
+    public List<Usuario> index(){
+        return usuarios;
     }
 
 }
--- src/main/java/br/com/fiap/SystemManagement_V10/controllers/models/Usuario.java
+++ src/main/java/br/com/fiap/SystemManagement_V10/controllers/models/Usuario.java
@@ -1,58 +1,85 @@
 package br.com.fiap.SystemManagement_V10.controllers.models;
 
 public class Usuario {
+    
+    private Long id;
     private String login;
     private String nome;
     private String email;
     private String telefone;
     private String senha;
-
     
-    public Usuario(String login, String nome, String email, String telefone, String senha) {
+    
+    public Usuario(Long id, String login, String nome, String email, String telefone, String senha) {
+        this.id = id;
         this.login = login;
         this.nome = nome;
         this.email = email;
         this.telefone = telefone;
         this.senha = senha;
+
+
     }
+
+
+    public Long getId() {
+        return id;
+    }
+
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
 
     public String getLogin() {
         return login;
     }
 
+
     public void setLogin(String login) {
         this.login = login;
     }
+
 
     public String getNome() {
         return nome;
     }
 
+
     public void setNome(String nome) {
         this.nome = nome;
     }
+
 
     public String getEmail() {
         return email;
     }
 
+
     public void setEmail(String email) {
         this.email = email;
     }
+
 
     public String getTelefone() {
         return telefone;
     }
 
+
     public void setTelefone(String telefone) {
         this.telefone = telefone;
     }
+
 
     public String getSenha() {
         return senha;
     }
 
+
     public void setSenha(String senha) {
         this.senha = senha;
     }
+    
+
 }
