74dd981040b38c5711ca14a30f6983d813469d0e
Gustavo Moura <100175494+mouragus@users.noreply.github.com>
2023-03-27 23:25:07 -0300

    Update Tarefa.java
    corrigindo erros

--- src/main/java/br/com/fiap/SystemManagement_V10/models/Tarefa.java
+++ src/main/java/br/com/fiap/SystemManagement_V10/models/Tarefa.java
@@ -1,9 +1,15 @@
 package br.com.fiap.SystemManagement_V10.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import java.time.LocalDate;
 
+@Entity
 public class Tarefa {
-
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome; 
     private String descricao;
