fb306abca0a9431e6b5fb0c6b8e73a11c2d06add
mouragus <gumora18@hotmail.com>
2023-03-28 00:16:27 -0300

    Subindo alterações na STG

--- src/main/java/br/com/fiap/SystemManagement_V10/models/Tarefa.java
+++ src/main/java/br/com/fiap/SystemManagement_V10/models/Tarefa.java
@@ -2,11 +2,29 @@ package br.com.fiap.SystemManagement_V10.models;
 
 import java.time.LocalDate;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
 public class Tarefa {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    
+    @NotEmpty
     private String nome; 
+    
+    @NotBlank @Size(min = 5, max = 250)
     private String descricao;
+    
+    @NotNull
     private LocalDate data;
         
     public Tarefa(String nome, String descricao, LocalDate data) {
--- src/main/java/br/com/fiap/SystemManagement_V10/models/Usuario.java
+++ src/main/java/br/com/fiap/SystemManagement_V10/models/Usuario.java
@@ -1,9 +1,14 @@
 package br.com.fiap.SystemManagement_V10.models;
 
+
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 @Entity
 public class Usuario {
@@ -11,10 +16,20 @@ public class Usuario {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    
+    @NotBlank
     private String login;
+    
+    @NotBlank @Size(min = 5, max = 35)
     private String nome;
+    
+    @NotNull
     private String email;
+    
+    @NotBlank @Size(min = 10, max = 11)
     private String telefone;
+    
+    @NotBlank
     private String senha;
 
     protected Usuario (){}
