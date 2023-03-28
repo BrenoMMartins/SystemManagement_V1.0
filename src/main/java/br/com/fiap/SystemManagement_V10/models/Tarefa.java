35d2c5534f2caf56d9a5f0d9cb68b9f45ee0adcc
mouragus <gumora18@hotmail.com>
2023-03-28 00:17:13 -0300

    corrigindo erros

--- src/main/java/br/com/fiap/SystemManagement_V10/models/Tarefa.java
+++ src/main/java/br/com/fiap/SystemManagement_V10/models/Tarefa.java
@@ -1,6 +1,7 @@
 package br.com.fiap.SystemManagement_V10.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import java.time.LocalDate;
